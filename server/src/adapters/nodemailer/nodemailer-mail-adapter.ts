e36e3479839255c36e7b82c081af41ad08b6ce7c
Erick Trettel <ericktrettel@gmail.com>
2022-05-08 16:20:40 -0300

    add server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'effcee02ebb170',
+    pass: 'c97b5fb6ff2ab5',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Erick Trettel <ericktrettel@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
